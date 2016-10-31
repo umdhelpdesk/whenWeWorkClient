ada240bbef6e36986bb9dd448a456edc0c32044b
William Kwao <williamkwao@gmail.com>
2016-10-31 10:39:39 -0400

    Update constants.js

--- constants/constants.js
+++ constants/constants.js
@@ -4,5 +4,5 @@ angular.module('hdsp')
 })
 .constant('API_ENDPOINT', {
   //url: 'http://localhost:8080/api'
-  url: 'http://ec2-52-35-4-4.us-west-2.compute.amazonaws.com:8080/api'
+  url: 'http://ec2-35-162-0-205.us-west-2.compute.amazonaws.com:8080/api'
 });
